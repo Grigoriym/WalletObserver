933f6d385bade304e4d1983fd325d0bcc7fd2ddf
Grigoriy Mikhalchuk <grigoriymikhalchuk@yahoo.com>
2019-06-19 17:57:07 +0600

    intro ui

--- app/src/main/java/com/example/walletobserver/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/example/walletobserver/ui/intro/IntroActivity.kt
@@ -1,10 +1,15 @@
 package com.example.walletobserver.ui.intro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.walletobserver.R
+import com.example.walletobserver.util.PrefsManager
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class IntroActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity(), KoinComponent {
+
+  private val prefs: PrefsManager by inject()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
