20aba5e81d0bb2baefcdcaeefc1e2429813729bd
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-07-06 10:33:52 +0600

    goals pager adapter init

--- app/src/main/java/com/example/walletobserver/ui/goals/GoalsFragment.kt
+++ app/src/main/java/com/example/walletobserver/ui/goals/GoalsFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.walletobserver.R
+import kotlinx.android.synthetic.main.fragment_goals.*
+import timber.log.Timber
 
 class GoalsFragment : Fragment() {
 
@@ -14,5 +16,13 @@ class GoalsFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? =
     inflater.inflate(R.layout.fragment_goals, container, false)
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    Timber.d("GoalsFragment - OnViewCreated")
+    val gpa = GoalsPagerAdapter(activity?.supportFragmentManager)
+    vpGoals.adapter = gpa
+    tlGoals.setupWithViewPager(vpGoals)
+  }
 
 }
