b712de700ebe6e4a80186fa4992b1a13bd1f9d34
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-07-06 12:04:33 +0600

    layouts for sectioned rv

--- app/src/main/java/com/example/walletobserver/ui/records/RecordsFragment.kt
+++ app/src/main/java/com/example/walletobserver/ui/records/RecordsFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.example.walletobserver.R
+import kotlinx.android.synthetic.main.fragment_records.*
+import timber.log.Timber
 
 class RecordsFragment : Fragment() {
 
@@ -15,5 +18,14 @@ class RecordsFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? =
     inflater.inflate(R.layout.fragment_records, container, false)
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    Timber.d("RecordsFragment - onViewCreated")
+
+    fabRecords.setOnClickListener {
+      findNavController().navigate(RecordsFragmentDirections.nextFragment())
+    }
+  }
 
 }
---
+++ app/src/main/java/com/example/walletobserver/widgets/SectionedRecyclerView.kt
@@ -0,0 +1,4 @@
+package com.example.walletobserver.widgets
+
+class SectionedRecyclerView {
+}
