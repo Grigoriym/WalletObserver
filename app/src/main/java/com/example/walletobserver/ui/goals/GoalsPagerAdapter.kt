87f0622b02796ae26599c4393b4bb906009b55e2
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-07-06 10:03:07 +0600

    goals pager adapter

---
+++ app/src/main/java/com/example/walletobserver/ui/goals/GoalsPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.walletobserver.ui.goals
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.walletobserver.ui.goals.active.ActiveGoalsFragment
+import com.example.walletobserver.ui.goals.paused.PausedGoalsFragment
+import com.example.walletobserver.ui.goals.reached.ReachedGoalsFragment
+
+class GoalsPagerAdapter(
+  fragmentManager: FragmentManager?
+) : FragmentStatePagerAdapter(fragmentManager!!, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+  override fun getItem(position: Int): Fragment {
+    return when (position) {
+      0 -> ActiveGoalsFragment()
+      1 -> PausedGoalsFragment()
+      else -> ReachedGoalsFragment()
+    }
+  }
+
+  override fun getCount(): Int = 3
+
+  override fun getPageTitle(position: Int): CharSequence? {
+    return when (position) {
+      0 -> "Active"
+      1 -> "Paused"
+      else -> "Reached"
+    }
+  }
+
+}
