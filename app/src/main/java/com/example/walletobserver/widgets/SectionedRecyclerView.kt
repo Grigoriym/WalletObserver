b921349db8ad4f2c261866b1fe18d03b6d752c11
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-07-06 13:17:33 +0600

    sectioned rv

--- app/src/main/java/com/example/walletobserver/widgets/SectionedRecyclerView.kt
+++
@@ -1,4 +0,0 @@
-package com.example.walletobserver.widgets
-
-class SectionedRecyclerView {
-}
---
+++ app/src/main/java/com/example/walletobserver/widgets/SectionedRecyclerViewAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.walletobserver.widgets
+
+import androidx.recyclerview.widget.RecyclerView
+
+class SectionedRecyclerViewAdapter(
+
+) {
+}
