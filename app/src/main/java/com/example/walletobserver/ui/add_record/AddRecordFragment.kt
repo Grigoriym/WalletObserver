446e86629267b53ceb070d7016adb30b9cdb224e
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-07-07 10:35:19 +0600

    add record some ui

--- app/src/main/java/com/example/walletobserver/ui/add_record/AddRecordFragment.kt
+++ app/src/main/java/com/example/walletobserver/ui/add_record/AddRecordFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.walletobserver.R
+import kotlinx.android.synthetic.main.fragment_add_record.*
+import timber.log.Timber
 
 class AddRecordFragment : Fragment() {
 
@@ -15,5 +17,27 @@ class AddRecordFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? =
     inflater.inflate(R.layout.fragment_add_record, container, false)
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    Timber.d("AddRecordFragment - onViewCreated")
+
+    initButtons()
+  }
+
+  private fun initButtons(){
+    btnZeroAddRecord.setOnClickListener{
+      tvAddRecord.text = "${tvAddRecord.text}0"
+    }
+    btnOneAddRecord.setOnClickListener {
+      tvAddRecord.text = "${tvAddRecord.text}1"
+    }
+    btnTwoAddRecord.setOnClickListener {
+      tvAddRecord.text = "${tvAddRecord.text}2"
+    }
+    btnDeleteAddRecord.setOnClickListener {
+      tvAddRecord.text = tvAddRecord.text.dropLast(1)
+    }
+  }
 
 }
